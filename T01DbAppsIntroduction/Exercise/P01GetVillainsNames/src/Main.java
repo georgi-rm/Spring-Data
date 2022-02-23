4e084a02f1342dff0c3778bc597a4ee14b2d1abe
georgi-rm <georgi.rm@gmail.com>
2022-02-23 22:24:17 +0200

    Problem 1 "Get Villains Names" from Exercise: Java DB Apps Introduction

---
+++ T01DbAppsIntroduction/Exercise/P01GetVillainsNames/src/Main.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Properties properties = new Properties();
+        properties.setProperty("user", "guest_user");
+        properties.setProperty("password", "DummyPassword");
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/minions_db", properties);
+
+        PreparedStatement statement = connection.prepareStatement(
+                "SELECT v.name, count(DISTINCT mv.minion_id) AS 'count_of_minions' " +
+                        "FROM `villains` AS v " +
+                        "        JOIN " +
+                        "    `minions_villains` AS mv ON v.`id` = mv.`villain_id` " +
+                        "GROUP BY v.`id` " +
+                        "HAVING `count_of_minions` > 15 " +
+                        "ORDER BY `count_of_minions` DESC;");
+
+        ResultSet resultSet = statement.executeQuery();
+
+        if (resultSet.next()) {
+            String villainName = resultSet.getString("name");
+            String minionsCount = resultSet.getString("count_of_minions");
+            System.out.printf("%s %s", villainName, minionsCount);
+        }
+
+        connection.close();
+    }
+}
