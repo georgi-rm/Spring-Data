c20ab40d38149232921e4d4d4ce3773922d05f06
georgi-rm <georgi.rm@gmail.com>
2022-02-26 16:07:09 +0200

    Problem 2 "Get Minion Names" from Exercise: Java DB Apps Introduction

--- T01DbAppsIntroduction/Exercise/P02GetMinionNames/src/Main.java
+++ T01DbAppsIntroduction/Exercise/P02GetMinionNames/src/Main.java
@@ -47,7 +47,7 @@ public class Main {
         while (resultSet.next()) {
             String minionName = resultSet.getString("name");
             int minionAge = resultSet.getInt("age");
-            System.out.printf("%d. %s %s%n", number, minionName, minionAge);
+            System.out.printf("%d. %s %d%n", number, minionName, minionAge);
             number++;
         }
 
