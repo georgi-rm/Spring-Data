6718b88784ecea97e6ad1aeeb64baf63b9f91368
georgi-rm <georgi.rm@gmail.com>
2022-02-28 15:18:31 +0200

    Problem 1 "Setup" from Exercise: Introduction to Hibernate

--- T3IntroductionToHibernate/Exercise/P01Setup/src/main/java/Main.java
+++ T3IntroductionToHibernate/Exercise/P01Setup/src/main/java/Main.java
@@ -6,5 +6,8 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("PU_Name");
         EntityManager entityManager = factory.createEntityManager();
+
+        entityManager.close();
+        factory.close();
     }
 }
