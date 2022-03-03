eddd226d42f2b02f5342dd29c7c15b6c09f34f83
georgi-rm <georgi.rm@gmail.com>
2022-03-03 21:05:18 +0200

    Problem 9 "Find Latest 10 Projects" from Exercise: Introduction to Hibernate

---
+++ T03IntroductionToHibernate/Exercise/P09FindLatest10Projects/src/main/java/Main.java
@@ -0,0 +1,41 @@
+import entities.Employee;
+import entities.Project;
+import org.hibernate.Hibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("PU_Name");
+        EntityManager entityManager = factory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        List<Project> projects = entityManager.createQuery("SELECT p FROM Project AS p" +
+                        " order by p.startDate DESC, p.name ASC", Project.class)
+                .setMaxResults(10)
+                .getResultList();
+
+        entityManager.getTransaction().commit();
+
+        for (Project project : projects) {
+            System.out.printf("Project name: %s%n", project.getName());
+            System.out.printf(" \tProject Description: %s%n", project.getDescription());
+
+            DateTimeFormatter customFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss.0");
+
+            System.out.printf(" \tProject Start Date:%s%n",
+                    project.getStartDate().format(customFormatter));
+            System.out.printf(" \tProject End Date: %s%n",
+                    (project.getEndDate() == null) ? "null" : project.getEndDate().format(customFormatter));
+        }
+
+        entityManager.close();
+        factory.close();
+    }
+}
