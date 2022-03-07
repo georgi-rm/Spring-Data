72c42cc734733de63c5097d1066aaccce5de57ad
georgi-rm <georgi.rm@gmail.com>
2022-03-07 23:17:06 +0200

    Problem 1 "Project Setup" from Lab: Spring Data Intro

---
+++ T05SpringDataIntro/Lab/P01ProjectSetup/src/main/java/com/example/p01projectsetup/P01ProjectSetupApplication.java
@@ -0,0 +1,13 @@
+package com.example.p01projectsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P01ProjectSetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P01ProjectSetupApplication.class, args);
+    }
+
+}
