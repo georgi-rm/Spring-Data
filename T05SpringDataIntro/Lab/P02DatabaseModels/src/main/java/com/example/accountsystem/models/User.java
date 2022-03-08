168acb23c70286e589d247be02189b5ddb3362bb
georgi-rm <georgi.rm@gmail.com>
2022-03-08 23:56:06 +0200

    Problem 2 "Database Models" from Lab: Spring Data Intro

---
+++ T05SpringDataIntro/Lab/P02DatabaseModels/src/main/java/com/example/accountsystem/models/User.java
@@ -0,0 +1,63 @@
+package com.example.accountsystem.models;
+
+import javax.persistence.*;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private int age;
+
+    @OneToMany(targetEntity = Account.class, mappedBy = "user")
+    private Set<Account> accounts = new LinkedHashSet<>();
+
+    public User() {
+    }
+
+    public User(String username, int age, Set<Account> accounts) {
+        this.username = username;
+        this.age = age;
+        this.accounts = accounts;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Set<Account> getAccounts() {
+        return Collections.unmodifiableSet(accounts);
+    }
+
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
+    }
+}
