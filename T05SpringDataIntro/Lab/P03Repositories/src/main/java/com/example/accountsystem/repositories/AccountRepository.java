c7ac3a64376222054f8dc2ddcd7e81dfb0e8c056
georgi-rm <georgi.rm@gmail.com>
2022-03-09 07:49:06 +0200

    Problem 3 "Repositories" from Lab: Spring Data Intro

---
+++ T05SpringDataIntro/Lab/P03Repositories/src/main/java/com/example/accountsystem/repositories/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.example.accountsystem.repositories;
+
+import com.example.accountsystem.models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account findAccountById(Long id);
+}
