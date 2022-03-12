daf532cff0c495377e6e7e8fcc98cf924a452f51
georgi-rm <georgi.rm@gmail.com>
2022-03-12 21:00:05 +0200

    Problem 5 "Select Ingredients by Names" from Lab: Spring Data Advanced Querying

---
+++ T06SpringDataAdvancedQuering/Lab/P05SelectIngredientsByNames/src/main/java/com/example/advquerying/ConsoleRunner.java
@@ -0,0 +1,36 @@
+package com.example.advquerying;
+
+import com.example.advquerying.services.IngredientService;
+import com.example.advquerying.services.ShampooService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+@Component
+public class ConsoleRunner implements CommandLineRunner {
+
+    private final ShampooService shampooService;
+
+    private final IngredientService ingredientService;
+
+    @Autowired
+    public ConsoleRunner(ShampooService shampooService, IngredientService ingredientService) {
+        this.shampooService = shampooService;
+        this.ingredientService = ingredientService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Ingredient Names:");
+        List<String> names = Arrays.stream(scanner.nextLine().split("\\s+")).toList();
+
+        this.ingredientService.findIngredientsByNames(names)
+                .forEach(System.out::println);
+    }
+}
---
+++ T06SpringDataAdvancedQuering/Lab/P05SelectIngredientsByNames/src/main/java/com/example/advquerying/services/IngredientServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.advquerying.services;
+
+import com.example.advquerying.entities.Ingredient;
+import com.example.advquerying.repositories.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class IngredientServiceImpl implements IngredientService {
+
+    private final IngredientRepository ingredientRepository;
+
+    @Autowired
+    public IngredientServiceImpl(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @Override
+    public List<Ingredient> findIngredientsByName(String startingLetter) {
+        return ingredientRepository.findAllByNameStartingWith(startingLetter);
+    }
+
+    @Override
+    public List<Ingredient> findIngredientsByNames(List<String> names) {
+        return ingredientRepository.findAllByNameInOrderByPriceAsc(names);
+    }
+}
