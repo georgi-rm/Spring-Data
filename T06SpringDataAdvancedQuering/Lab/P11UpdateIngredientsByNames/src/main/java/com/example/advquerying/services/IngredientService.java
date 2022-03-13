c616dc9da162953b4d34803c875df00de21592fd
georgi-rm <georgi.rm@gmail.com>
2022-03-13 23:29:35 +0200

    Problem 11 "Update Ingredients by Names" from Lab: Spring Data Advanced Querying

---
+++ T06SpringDataAdvancedQuering/Lab/P11UpdateIngredientsByNames/src/main/java/com/example/advquerying/services/IngredientService.java
@@ -0,0 +1,19 @@
+package com.example.advquerying.services;
+
+import com.example.advquerying.entities.Ingredient;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface IngredientService {
+    List<Ingredient> findIngredientsByName(String startingLetter);
+
+    List<Ingredient> findIngredientsByNames(List<String> names);
+
+    int deleteIngredientByName(String name);
+
+    int increasePriceForAllIngredients();
+
+    int increasePriceForChosenItems(List<String> names);
+}
