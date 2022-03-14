5b9aabf7abe5ff3da7c368a41952663454a67b31
georgi-rm <georgi.rm@gmail.com>
2022-03-14 21:29:21 +0200

    Problem 8 "Book Titles Search" from Exercise: Spring Data Advanced Querying

---
+++ T06SpringDataAdvancedQuering/Exercise/P08BookTitlesSearch/src/main/java/com/example/springintro/repository/BookRepository.java
@@ -0,0 +1,29 @@
+package com.example.springintro.repository;
+
+import com.example.springintro.model.entity.AgeRestriction;
+import com.example.springintro.model.entity.Book;
+import com.example.springintro.model.entity.EditionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findAllByReleaseDateBefore(LocalDate releaseDateBefore);
+
+    List<Book> findAllByAgeRestriction(AgeRestriction ageRestriction);
+
+    List<Book> findAllByEditionTypeAndCopiesLessThan(EditionType editionType, int numberOfCopies);
+
+    List<Book> findAllByPriceLessThanOrPriceGreaterThan(BigDecimal price, BigDecimal price2);
+
+    List<Book> findAllByReleaseDateBeforeOrReleaseDateAfter(LocalDate firstDayOfYear, LocalDate lastDayOfYear);
+
+    List<Book> findAllByTitleContaining(String textToSearch);
+
+    List<Book> findAllByAuthorLastNameStartingWith(String textToSearch);
+}
