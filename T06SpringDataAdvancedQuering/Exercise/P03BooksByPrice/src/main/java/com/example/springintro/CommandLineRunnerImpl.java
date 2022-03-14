874e09929fcb831edb87b34009d0a2359652737f
georgi-rm <georgi.rm@gmail.com>
2022-03-14 20:27:03 +0200

    Problem 3 "Books by Price" from Exercise: Spring Data Advanced Querying

---
+++ T06SpringDataAdvancedQuering/Exercise/P03BooksByPrice/src/main/java/com/example/springintro/CommandLineRunnerImpl.java
@@ -0,0 +1,54 @@
+package com.example.springintro;
+
+import com.example.springintro.model.entity.Book;
+import com.example.springintro.model.entity.EditionType;
+import com.example.springintro.service.AuthorService;
+import com.example.springintro.service.BookService;
+import com.example.springintro.service.CategoryService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+
+    private final CategoryService categoryService;
+    private final AuthorService authorService;
+    private final BookService bookService;
+
+    public CommandLineRunnerImpl(CategoryService categoryService, AuthorService authorService, BookService bookService) {
+        this.categoryService = categoryService;
+        this.authorService = authorService;
+        this.bookService = bookService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        seedData();
+
+        printAllBooksByPrice();
+    }
+
+    private void printAllBooksByPrice() {
+        this.bookService.findAllBooksByPriceLowerThanAndHigherThan(5, 40)
+                .forEach(book -> System.out.printf("%s - $%s%n", book.getTitle(), book.getPrice()));
+    }
+
+    private void printAllBooksByEditionAndCopiesLessThan5000() {
+        this.bookService.findAllBooksByEditionAndCopies(EditionType.GOLD, 5000)
+                .forEach(book -> System.out.println(book.getTitle()));
+    }
+
+    private void printAllBookTitlesByAgeRestriction(String ageRestrictionText) {
+        this.bookService.findAllBooksByAgeRestriction(ageRestrictionText)
+                .forEach(book -> System.out.println(book.getTitle()));
+    }
+
+    private void seedData() throws IOException {
+        categoryService.seedCategories();
+        authorService.seedAuthors();
+        bookService.seedBooks();
+    }
+}
---
+++ T06SpringDataAdvancedQuering/Exercise/P03BooksByPrice/src/main/java/com/example/springintro/service/BookService.java
@@ -0,0 +1,17 @@
+package com.example.springintro.service;
+
+import com.example.springintro.model.entity.Book;
+import com.example.springintro.model.entity.EditionType;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface BookService {
+    void seedBooks() throws IOException;
+
+    List<Book> findAllBooksByAgeRestriction(String ageRestrictionText);
+
+    List<Book> findAllBooksByEditionAndCopies(EditionType editionType, int numberOfCopies);
+
+    List<Book> findAllBooksByPriceLowerThanAndHigherThan(double lower, double higher);
+}
