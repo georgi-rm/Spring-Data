e42c144e3c3a1051b5efbe2de7b0fbe10ebe056f
georgi-rm <georgi.rm@gmail.com>
2022-03-14 20:12:18 +0200

    Problem 2 "Golden Books" from Exercise: Spring Data Advanced Querying

---
+++ T06SpringDataAdvancedQuering/Exercise/P02GoldenBooks/src/main/java/com/example/springintro/repository/BookRepository.java
@@ -0,0 +1,18 @@
+package com.example.springintro.repository;
+
+import com.example.springintro.model.entity.AgeRestriction;
+import com.example.springintro.model.entity.Book;
+import com.example.springintro.model.entity.EditionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findAllByAgeRestriction(AgeRestriction ageRestriction);
+
+    List<Book> findAllByEditionTypeAndCopiesLessThan(EditionType editionType, int numberOfCopies);
+}
---
+++ T06SpringDataAdvancedQuering/Exercise/P02GoldenBooks/src/main/java/com/example/springintro/service/BookService.java
@@ -0,0 +1,15 @@
+package com.example.springintro.service;
+
+import com.example.springintro.model.entity.Book;
+import com.example.springintro.model.entity.EditionType;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface BookService {
+    void seedBooks() throws IOException;
+
+    List<Book> findAllBooksByAgeRestriction(String ageRestrictionText);
+
+    List<Book> findAllBooksByEditionAndCopies(EditionType editionType, int numberOfCopies);
+}
