2fa04a895d785aa0db401a0dd691477e07f5dc4d
georgi-rm <georgi.rm@gmail.com>
2022-03-14 20:58:10 +0200

    Problem 5 "Books Released Before Date" from Exercise: Spring Data Advanced Querying

---
+++ T06SpringDataAdvancedQuering/Exercise/P05BooksReleasedBeforeDate/src/main/java/com/example/springintro/CommandLineRunnerImpl.java
@@ -0,0 +1,69 @@
+package com.example.springintro;
+
+import com.example.springintro.model.entity.Book;
+import com.example.springintro.model.entity.EditionType;
+import com.example.springintro.service.AuthorService;
+import com.example.springintro.service.BookService;
+import com.example.springintro.service.CategoryService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+
+    private final CategoryService categoryService;
+    private final AuthorService authorService;
+    private final BookService bookService;
+
+    public CommandLineRunnerImpl(CategoryService categoryService, AuthorService authorService, BookService bookService) {
+        this.categoryService = categoryService;
+        this.authorService = authorService;
+        this.bookService = bookService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        seedData();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter date (DD-MM-YYYY):");
+        String date = scanner.nextLine();
+
+        printBooksReleasedBefore(date);
+    }
+
+    private void printBooksReleasedBefore(String date) {
+        this.bookService.findAllBooksReleasedBeforeDate(date)
+                .forEach(System.out::println);
+    }
+
+    private void printBookTitlesForBooksNotReleasedInYear(int year) {
+        this.bookService.findAllBooksNotInYear(year)
+                .forEach(book -> System.out.println(book.getTitle()));
+    }
+
+    private void printAllBooksByPrice() {
+        this.bookService.findAllBooksByPriceLowerThanAndHigherThan(5, 40)
+                .forEach(book -> System.out.printf("%s - $%s%n", book.getTitle(), book.getPrice()));
+    }
+
+    private void printAllBooksByEditionAndCopiesLessThan5000() {
+        this.bookService.findAllBooksByEditionAndCopies(EditionType.GOLD, 5000)
+                .forEach(book -> System.out.println(book.getTitle()));
+    }
+
+    private void printAllBookTitlesByAgeRestriction(String ageRestrictionText) {
+        this.bookService.findAllBooksByAgeRestriction(ageRestrictionText)
+                .forEach(book -> System.out.println(book.getTitle()));
+    }
+
+    private void seedData() throws IOException {
+        categoryService.seedCategories();
+        authorService.seedAuthors();
+        bookService.seedBooks();
+    }
+}
---
+++ T06SpringDataAdvancedQuering/Exercise/P05BooksReleasedBeforeDate/src/main/java/com/example/springintro/service/BookService.java
@@ -0,0 +1,21 @@
+package com.example.springintro.service;
+
+import com.example.springintro.model.entity.Book;
+import com.example.springintro.model.entity.EditionType;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface BookService {
+    void seedBooks() throws IOException;
+
+    List<Book> findAllBooksByAgeRestriction(String ageRestrictionText);
+
+    List<Book> findAllBooksByEditionAndCopies(EditionType editionType, int numberOfCopies);
+
+    List<Book> findAllBooksByPriceLowerThanAndHigherThan(double lower, double higher);
+
+    List<Book> findAllBooksNotInYear(int year);
+
+    List<Book> findAllBooksReleasedBeforeDate(String date);
+}
