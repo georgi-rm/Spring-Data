cb215686643c427473f04665f4cfa6cd592d1767
georgi-rm <georgi.rm@gmail.com>
2022-03-19 23:14:48 +0200

    Problem 1 "Design the Database" from Exercise: Spring Data Auto Mapping Objects

---
+++ T07SpringDataAutoMappingObjects/Exercise/P01DesigntheDatabase/src/main/java/com/example/springintro/model/entity/Order.java
@@ -0,0 +1,35 @@
+package com.example.springintro.model.entity;
+
+import com.example.springintro.model.entity.users.User;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private User buyer;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Game> products;
+
+    public Order() {}
+
+    public Order(User buyer, Set<Game> products) {
+        this.buyer = buyer;
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
